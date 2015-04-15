16245d8460c555b78802ed4312882073574a5cf5
Yung Dai <yungchidai@gmail.com>
2015-04-15 11:53:17 -0400

    Initial Commit

---
+++ UINavigationAndButtons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UINavigationAndButtons
+//
+//  Created by Yung Dai on 2015-04-15.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UINavigationAndButtons/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UINavigationAndButtons
+//
+//  Created by Yung Dai on 2015-04-15.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
